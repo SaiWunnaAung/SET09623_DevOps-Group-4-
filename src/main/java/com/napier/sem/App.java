fe962abb2c3cb84f6d338d825c253eb298f57ea4
kyawhein1802 <40501802@live.napier.ac.uk>
2022-03-23 18:46:52 +0630

    All the countries in a continent organised by largest population to smallest.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -72,8 +72,8 @@ public class App
             for(int i=0; i<reportNo.length;i++){
                 System.out.println(reportNo[i]);
             }
-        System.out.println("Choose Report Number: 1");
-            chooseMenu(1);
+        System.out.println("Choose Report Number: 2");
+            chooseMenu(2);
 
     }
 
