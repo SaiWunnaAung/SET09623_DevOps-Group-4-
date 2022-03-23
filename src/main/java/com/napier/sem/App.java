90f18b4643d8752fd0fbbf564a6c3a9bc4fb6361
akm2121 <40501795@live.napier.ac.uk>
2022-03-23 18:59:47 +0630

    The population of a country.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -72,8 +72,8 @@ public class App
             for(int i=0; i<reportNo.length;i++){
                 System.out.println(reportNo[i]);
             }
-        System.out.println("Choose Report Number: 28");
-            chooseMenu(28);
+        System.out.println("Choose Report Number: 29");
+            chooseMenu(29);
 
     }
 
