36f0915d5b525e18ee89e1d298b4355b23ae2e59
SaiWunnaAung <40501792@live.napier.ac.uk>
2022-03-23 18:14:58 +0630

    All the cities in a continent organised by largest population to smallest.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -72,8 +72,8 @@ public class App
             for(int i=0; i<reportNo.length;i++){
                 System.out.println(reportNo[i]);
             }
-        System.out.println("Choose Report Number: 7");
-            chooseMenu(7);
+        System.out.println("Choose Report Number: 8");
+            chooseMenu(8);
 
     }
 
