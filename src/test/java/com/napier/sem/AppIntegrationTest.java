5ee54aa1ba16c73ffae344128d16fec3caadfabf
kyawhein1802 <40501802@live.napier.ac.uk>
2022-04-01 09:31:03 +0630

    Add integration test for country report 3 and 4

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -97,6 +97,66 @@ public class AppIntegrationTest {
     }
 
     /**
+     * This method is integration test for Country Report Three
+     * @throws SQLException
+     */
+    @Test
+    void testCountryReportThree() throws SQLException {
+        // create array list
+        ArrayList<Country> countries = db.countryReportThree("Eastern Asia");// create array list
+        // check array list is null
+        assertNotNull(countries);
+        // check array list has data
+        assertEquals(countries.size() > 0, true);
+        /*Check data items are not null*/
+        assertNotNull(countries.get(0).getCode());
+        assertNotNull(countries.get(0).getName());
+        assertNotNull(countries.get(0).getContinent());
+        assertNotNull(countries.get(0).getRegion());
+        assertNotNull(countries.get(0).getPopulation());
+        assertNotNull(countries.get(0).getCapital());
+        /*Check expected data are correct or not*/
+        assertEquals(countries.get(0).getCode(),"CHN");
+        assertEquals(countries.get(0).getName(),"China");
+        assertEquals(countries.get(0).getContinent(),"Asia");
+        assertEquals(countries.get(0).getRegion(),"Eastern Asia");
+        assertEquals(countries.get(0).getPopulation(),1277558000);
+        assertEquals(countries.get(0).getCapital(),"Peking");
+        // print report
+        report.countryReportTemplate(countries);
+    }
+
+    /**
+     * This method is integration test for Country Report Four
+     * @throws SQLException
+     */
+    @Test
+    void testCountryReportFour() throws SQLException {
+        // create array list
+        ArrayList<Country> countries = db.countryReportFour(5);
+        // check array list is null
+        assertNotNull(countries);
+        // check array list has data
+        assertEquals(countries.size() > 0, true);
+        /*Check data items are not null*/
+        assertNotNull(countries.get(0).getCode());
+        assertNotNull(countries.get(0).getName());
+        assertNotNull(countries.get(0).getContinent());
+        assertNotNull(countries.get(0).getRegion());
+        assertNotNull(countries.get(0).getPopulation());
+        assertNotNull(countries.get(0).getCapital());
+        /*Check expected data are correct or not*/
+        assertEquals(countries.get(0).getCode(),"CHN");
+        assertEquals(countries.get(0).getName(),"China");
+        assertEquals(countries.get(0).getContinent(),"Asia");
+        assertEquals(countries.get(0).getRegion(),"Eastern Asia");
+        assertEquals(countries.get(0).getPopulation(),1277558000);
+        assertEquals(countries.get(0).getCapital(),"Peking");
+        // print report
+        report.countryReportTemplate(countries);
+    }
+
+    /**
      * This method is integration test for City Report One
      * @throws SQLException
      */
