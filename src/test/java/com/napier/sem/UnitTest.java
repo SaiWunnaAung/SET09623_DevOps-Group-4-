9457f0c6144511b10cd0b0b11191738fcc87fec5
SaiWunnaAung <40501792@live.napier.ac.uk>
2022-04-01 08:49:47 +0630

    Add unit test for city reports.

--- src/test/java/com/napier/sem/UnitTest.java
+++ src/test/java/com/napier/sem/UnitTest.java
@@ -1,13 +1,51 @@
 package com.napier.sem;
 
-import org.junit.jupiter.api.Test;
+import com.napier.sem.model.CapitalCity;
+import com.napier.sem.model.City;
+import com.napier.sem.model.Country;
+import com.napier.sem.model.Population;
+import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
 
-public class UnitTest {
-    @Test
-    void unitTest()
+
+/**
+ * This class is for Unit Testing
+ */
+class UnitTest
+{
+    // Define report object
+    static Report report;
+
+    /**
+     *  This method is to run before all test
+     */
+    @BeforeAll
+    static void init()
     {
-        assertEquals(5, 5);
+        // Create report object
+        report = new Report();
     }
+
+    /**
+     * This method is test method for City Reports
+     */
+    @Test
+    void uniTestCityReport()
+    {
+        // Create cities Array List
+        ArrayList<City> cities = new ArrayList<City>();
+        /* Create city Object and set data to setter methods*/
+        City city = new City();
+        city.setName("Tokyo");
+        city.setCountry("Japan");
+        city.setDistrict("Tokyo-to");
+        city.setPopulation(7980230);
+        // Push city object into cities array list
+        cities.add(city);
+        // Print city report
+        report.cityReportTemplate(cities);
+    }
+    
+
 }
