35963c32e747637ced2eb266246a68596c82a957
kyawhein1802 <40501802@live.napier.ac.uk>
2022-04-06 20:20:27 +0630

    Edit some error in city report.

--- src/main/java/com/napier/sem/Report.java
+++ src/main/java/com/napier/sem/Report.java
@@ -1,8 +1,6 @@
 package com.napier.sem;
-import com.napier.sem.model.CapitalCity;
 import com.napier.sem.model.City;
-import com.napier.sem.model.Country;
-import com.napier.sem.model.Population;
+
 import java.util.ArrayList;
 
 /**
@@ -10,33 +8,6 @@ import java.util.ArrayList;
  */
 public class Report {
 
-    /**
-     * Methods for countryReportTemplate with table formatted
-     * @param country Arraylist
-     */
-    public void countryReportTemplate(ArrayList<Country> country){
-        //Print report title
-        System.out.format("%-9s %-9s  %-40s  %-30.25s  %-30s  %-20s  %-20s", "No", "CODE", "NAME", "CONTINENT", "REGION", "POPULATION", "CAPITAL");
-        System.out.println("\n");
-        // Define counter variable
-        int count = 0;
-        // Loop and Retrieve data from country array list as a country object
-        for (Country countries:country){
-            // Increase count
-            count++;
-            // Print report by formatted design
-            System.out.printf("%-9s %-9s  %-40s  %-30.25s  %-30s  %-20s  %-20s",
-                    count,
-                    countries.getCode(),
-                    countries.getName(),
-                    countries.getContinent(),
-                    countries.getRegion(),
-                    countries.getPopulation(),
-                    countries.getCapital()
-            );
-            System.out.println("\n");
-        }
-    }
 
     /**
      * Methods for cityReportTemplate with table formatted
@@ -59,108 +30,6 @@ public class Report {
                     cities.getCountry(),
                     cities.getDistrict(),
                     cities.getPopulation()
-            );
-            System.out.println("\n");
-        }
-    }
-
-    /**
-     * Methods for capitalCityReportTemplate with table formatted
-     * @param capitalCity Arraylist
-     */
-    public void capitalCityReportTemplate(ArrayList<CapitalCity> capitalCity){
-        //Print report title
-        System.out.format("%-9s %-30.25s  %-30s  %-20s", "No",  "NAME", "CONTINENT","POPULATION");
-        System.out.println("\n");
-        // Define counter variable
-        int count = 0;
-        // Loop and Retrieve data from capitalCity array list as a CapitalCity object
-        for (CapitalCity capitalCities:capitalCity){
-            // Increase count
-            count++;
-            // Print report by formatted design
-            System.out.printf("%-9s %-30.25s  %-30s  %-20s",
-                    count,
-                    capitalCities.getName(),
-                    capitalCities.getCountry(),
-                    capitalCities.getPopulation()
-            );
-            System.out.println("\n");
-        }
-    }
-
-    /**
-     * Methods for populationReportTemplate with table formatted
-     * @param population Arraylist
-     */
-    public void populationReportTemplate(ArrayList<Population> population){
-        //Print report title
-        System.out.format("%-9s %-35s  %-35s  %-35s  %-35s  %-35s  %-35s", "No",  "NAME", "POPULATION", "POPULATION IN CITY", "POPULATION NOT IN CITY", "POPULATION IN CITY PERCENTAGE", "POPULATION NOT IN CITY PERCENTAGE");
-        System.out.println("\n");
-        // Define counter variable
-        int count = 0;
-        // Loop and Retrieve data from population array list as a Population object
-        for (Population populations:population){
-            // Increase count
-            count++;
-            // Print report by formatted design
-            System.out.printf("%-9s %-35s  %-35s  %-35s  %-35s  %-35s  %-35s",
-                    count,
-                    populations.getName(),
-                    populations.getPopulation(),
-                    populations.getPopInCity(),
-                    populations.getPopNotInCity(),
-                    populations.getPercentagePopulationInCities(),
-                    populations.getPercentagePopulationNotInCities()
-            );
-            System.out.println("\n");
-        }
-    }
-
-    /**
-     * Methods for simplePopulationReportTemplate with table formatted
-     * @param population Arraylist
-     */
-    public void simplePopulationReportTemplate(ArrayList<Population> population){
-        //Print report title
-        System.out.format("%-9s %-30.25s  %-20s", "No",  "NAME", "POPULATION");
-        System.out.println("\n");
-        // Define counter variable
-        int count = 0;
-        // Loop and Retrieve data from population array list as a Population object
-        for (Population populations:population){
-            // Increase count
-            count++;
-            // Print report by formatted design
-            System.out.printf("%-9s %-30.25s  %-20s",
-                    count,
-                    populations.getName(),
-                    populations.getPopulation()
-            );
-            System.out.println("\n");
-        }
-    }
-
-    /**
-     * Methods for advancePopulationReportTemplate with table formatted
-     * @param population Arraylist
-     */
-    public void advancePopulationReportTemplate(ArrayList<Population> population){
-        //Print report title
-        System.out.format("%-9s %-30.25s  %-37.25s  %-20s", "No",  "LANGUAGE NAME", "POPULATION NUMBER", "POPULATION PERCENTAGE (%)");
-        System.out.println("\n");
-        // Define counter variable
-        int count = 0;
-        // Loop and Retrieve data from population array list as a Population object
-        for (Population populations:population){
-            // Increase count
-            count++;
-            // Print report by formatted design
-            System.out.printf("%-9s %-30.25s  %-37.25s  %-20s",
-                    count,
-                    populations.getName(),
-                    populations.getPopulation(),
-                    populations.getPercentagePopulationInCities()
             );
             System.out.println("\n");
         }
--- src/test/java/com/napier/sem/UnitTest.java
+++ src/test/java/com/napier/sem/UnitTest.java
@@ -1,9 +1,6 @@
 package com.napier.sem;
 
-import com.napier.sem.model.CapitalCity;
 import com.napier.sem.model.City;
-import com.napier.sem.model.Country;
-import com.napier.sem.model.Population;
 import org.junit.jupiter.api.*;
 
 import java.util.ArrayList;
@@ -27,27 +24,7 @@ class UnitTest
         report = new Report();
     }
 
-    /**
-     * This method is test method for City Reports
-     */
-    @Test
-    void uniTestCountryReport()
-    {
-        // Create countries Array List
-        ArrayList<Country> countries = new ArrayList<Country>();
-        /* Create Country Object and set data to setter methods*/
-        Country country = new Country();
-        country.setCode("CHN");
-        country.setName("China");
-        country.setContinent("Asia");
-        country.setRegion("Eastern Asia");
-        country.setPopulation(1277558000);
-        country.setCapital("Peking");
-        // Push country object into countries array list
-        countries.add(country);
-        // Print country report
-        report.countryReportTemplate(countries);
-    }
+
 
     /**
      * This method is test method for City Reports
@@ -68,88 +45,5 @@ class UnitTest
         // Print city report
         report.cityReportTemplate(cities);
     }
-
-    /**
-     * This method is test method for Capital City Reports
-     */
-    @Test
-    void uniTestCapitalCityReport()
-    {
-        // Create capitalCities Array List
-        ArrayList<CapitalCity> capitalCities = new ArrayList<CapitalCity>();
-        /* Create capitalCity Object and set data to setter methods*/
-        CapitalCity capitalCity = new CapitalCity();
-        capitalCity.setName("Tokyo");
-        capitalCity.setCountry("Japan");
-        capitalCity.setPopulation(7980230);
-        // Push capitalCity object into capitalCities array list
-        capitalCities.add(capitalCity);
-        // Print capitalCity report
-        report.capitalCityReportTemplate(capitalCities);
-    }
-
-    /**
-     * This method is test method for Population Reports
-     */
-    @Test
-    void uniTestPopulationReport()
-    {
-        // Create populations Array List
-        ArrayList<Population> populations = new ArrayList<Population>();
-        /* Create population Object and set data to setter methods*/
-        Population population = new Population();
-        population.setName("Caribbean");
-        population.setPopulation(38140000);
-        population.setPopInCity(11067550);
-        population.setPopNotInCity(27072450);
-        population.setPercentagePopulationInCities(29.01);
-        population.setPercentagePopulationNotInCities(70.98);
-        // Push population object into populations array list
-        populations.add(population);
-        // Print population report
-        report.populationReportTemplate(populations);
-    }
-
-    /**
-     * This method is test method for Additional Reports
-     */
-    @Test
-    void uniTestAdditionalReport()
-    {
-        // Create populations Array List
-        ArrayList<Population> populations = new ArrayList<Population>();
-        /* Create population Object and set data to setter methods*/
-        Population population = new Population();
-        population.setName("World Population");
-        population.setPopulation(6078749450L);
-        // Push population object into populations array list
-        populations.add(population);
-        // Print population report
-        report.simplePopulationReportTemplate(populations);
-    }
-
-    /**
-     * This method is test method for Language Reports
-     */
-    @Test
-    void uniTestLanguageReport()
-    {
-        // Create populations Array List
-        ArrayList<Population> populations = new ArrayList<Population>();
-        /* Create population Object and set data to setter methods*/
-        Population population = new Population();
-        population.setName("Chinese");
-        population.setPopulation(119184353922L);
-        population.setPercentagePopulationInCities(19.60);
-        // Push population object into populations array list
-        populations.add(population);
-        // Print population report
-        report.advancePopulationReportTemplate(populations);
-    }
-
-
-
-
-
 
 }
